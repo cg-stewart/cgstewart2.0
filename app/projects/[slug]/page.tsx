f1090ab9114ef217a275bde80b5839d56c502bf6
Cameron Stewart <newrgm@NEWRGM.attlocal.net>
2025-02-11 13:40:57 -0600

    added placeholder data [slug] pages ..

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>Project detail page</h1>
+    </div>
+  );
+}
---
+++ app/videos/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>Video detail page</h1>
+    </div>
+  );
+}
