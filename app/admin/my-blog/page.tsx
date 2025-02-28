95dcadac6005286e147257d758a3c106d57c4cb8
Cameron Stewart <newrgm@NEWRGM.attlocal.net>
2025-02-28 14:40:55 -0600

    Remove unused files from app directory

--- app/admin/my-blog/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminBlogPage() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Admin Blog Page</h1>
-    </div>
-  );
-}
--- app/admin/my-media/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Admin Media Page</h1>
-    </div>
-  );
-}
